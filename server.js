366198b184b0d149706bea1561d5de05c5f9397d
DevFahad <120650748+Fahad4345@users.noreply.github.com>
2026-06-07 13:33:42 +0500

    Update server.js

--- server.js
+++ server.js
@@ -14,13 +14,24 @@ const app = express();
 
 // Middleware
 app.use(express.json());
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://fuuast-xi.vercel.app/"
+];
+
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:3000",
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
-  }),
+  })
 );
 app.use(cookieParser());
 
